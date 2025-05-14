90f0589db9c444f56fd37a3eb8fc73747fb92a70
이현영 <89445100+hamo-o@users.noreply.github.com>
2025-05-14 20:42:59 +0900

    [Feature] 액션시트 컴포넌트 구현 (#165)
    * feat: ActionSheet Context
    
    * feat: ActionSheet Header
    
    * feat: ActionSheet Body, Footer
    
    * feat: ActionSheet Background
    
    * feat: ActionSheet 컴포넌트
    
    * feat: 애니메이션 구현
    
    * feat: 액션시트 스토리북
    
    * feat: Overlay 이름 변경 및 빌드
    
    * feat: rest prop 추가 및 스토리북 추가
    
    * feat: children 순서 및 형태 제한
    
    * fix: body, footer Flex extends
    
    * refactor: 액션시트 닫는 함수 리팩터링
    
    * chore: 스토리북 Default 잘 보이도록 수정
    
    * feat: ActionSheet Context
    
    * feat: ActionSheet Header
    
    * feat: ActionSheet Body, Footer
    
    * feat: ActionSheet Background
    
    * feat: ActionSheet 컴포넌트
    
    * feat: 애니메이션 구현
    
    * feat: 액션시트 스토리북
    
    * feat: Overlay 이름 변경 및 빌드
    
    * feat: rest prop 추가 및 스토리북 추가
    
    * feat: children 순서 및 형태 제한
    
    * fix: body, footer Flex extends
    
    * refactor: 액션시트 닫는 함수 리팩터링
    
    * chore: 스토리북 Default 잘 보이도록 수정
    
    * fix: 모바일 뷰에서는 너비 100% 적용
    
    * fix: 외부 주입 className 명시적으로 병합
    
    * fix: 헤더부분 ReactNode로 받을 수 있도록, sub 옵셔널 변환
    
    * feat: overlay 토큰 조정, actionSheet 토큰 추가
    
    * fix: 하드코딩 토큰값 변경
    
    * fix: 매직넘버 삭제
    
    * feat: ref 주입할 수 있도록
    
    * docs: 스토리북 구체적으로 적기
    
    * chore: axe-playwright 버전업
    
    * fix: undefined 가드처리
    
    * fix: 토큰 추가
    
    * chore: 스토리북 test-runner 버전업
    
    * feat: 액션시트 스토리북 보강
    
    * feat: useMergeREfs 에서 함수형 ref 도 받을 수 있도록 수정
    
    * feat: 스토리북 8.6.0 버전업 및 changeset 추가
    
    * feat: 코드리뷰 반영
    
    * chore: onClose 의 정확한 타입 명시
    
    * chore: delay 시간 수정
    
    ---------
    
    Co-authored-by: SeieunYoo <isho48239@gmail.com>

--- packages/wow-ui/src/hooks/useMergeRefs.ts
+++ packages/wow-ui/src/hooks/useMergeRefs.ts
@@ -1,13 +1,24 @@
 import type { MutableRefObject, Ref } from "react";
+import { useRef } from "react";
 
-export function useMergeRefs<T = any>(...refs: (Ref<T> | null)[]) {
-  return (value: T | null) => {
-    refs.forEach((ref) => {
+export function useMergeRefs<T = any>(...refs: (Ref<T> | null | undefined)[]) {
+  const internalRef = useRef<T | null>(null);
+
+  const mergedRef = (value: T | null) => {
+    internalRef.current = value;
+
+    for (const ref of refs) {
+      if (!ref) continue;
       if (typeof ref === "function") {
         ref(value);
-      } else if (ref !== null && typeof ref === "object") {
+      } else if (typeof ref === "object") {
         (ref as MutableRefObject<T | null>).current = value;
       }
-    });
+    }
   };
+
+  (mergedRef as typeof mergedRef & { current: T | null }).current =
+    internalRef.current;
+
+  return mergedRef as typeof mergedRef & { current: T | null };
 }
